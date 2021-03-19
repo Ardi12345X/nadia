ca72f4d68d9ecf309aa27940c2e8ff7219bb8aac
MR M3R4Z <77550679+Ardi12345X@users.noreply.github.com>
2021-03-20 00:27:51 +0700

    Add files via upload

---
+++ spam.sh
@@ -0,0 +1,71 @@
+#jangan ganti author nya"
+#script ini hanya untuk hiburan aja"
+#subscribe chennel MR M3R4Z"
+
+sleep 4
+echo -e "\e[33mhalo\e[0m";
+sleep 5
+echo -e "\e[31mhecked by M3R4Z\e[0m";
+sleep 4
+echo -e "\e[94msubscribe chennel MR M3R4Z\e[0m";
+sleep 5
+echo -e "\e[92mAuthor=Ardy\e[0m";
+sleep 3
+echo -e "\e[36mno WA=+6283192644730\e[0m";
+sleep 2
+echo -e "\e[35mTIM=TCT=Termux•Cyber•Tim\e[0m";
+sleep 6
+echo -e "\e[33mjangan di salah gunakan!\e[0m";
+sleep 3
+echo -e "\e[32mmasukan no target\e[0m";
+read -p '¤-->' userinput1;
+#
+sleep 3
+echo -e "\e[34mmasukan jumlah spam\e[0m";
+sleep 6
+read -p '¤-->' userinput2;
+#
+sleep 6
+echo -e "\e[34mtunggu sebentar!\e[0m";
+sleep 9
+echo -e "\e[35mloading\e[0m";
+sleep 9
+echo -e "\e[34mloading\e[0m";
+sleep 6
+echo -e "loading";
+sleep 9
+echo -e "\e[33mloading\e[0m";
+sleep 3
+echo -e "\e[95mloading\e[0m";
+sleep 6
+echo -e "\e[91mloading\e[0m";
+sleep 6
+echo -e "\e[96mmasukan kembali no terget\e[0m";
+sleep 2
+read -p '¤-->' userinput3;
+#
+echo -e "\e[34mspam berhasil.....\e[0m";
+sleep 6
+echo -e "\e[95mspam berhasil.....\e[0m";
+sleep 6
+echo -e "\e[94mspam berhasil.....\e[0m";
+sleep 9
+echo -e "spam berhasil.....";
+sleep 6
+echo -e "\e[33mspam berhasil.....\e[0m";
+sleep 4
+echo -e "\e[32mspam berhasil.....\e[0m";
+sleep 6
+echo -e "\e[35mspam berhasil.....\e[0m";
+sleep 6
+echo -e "\e[34m....................\e[0m";
+sleep 3
+echo -e "\e[35mjika script tidak berpungsi berarti script sudah tidak work!\e[0m";
+sleep 6
+echo -e "\e[92msubscribe chennel MR M3R4Z\e[0m";
+sleep 3
+echo -e "\e[33mketik exit untuk keluar\e[0m";
+#logout
+read -p '¤-->' userinput4;
+#
+sleep 4
